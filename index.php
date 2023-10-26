ebf6b73234bfec5a21697b80bb0f7e903e04aca2
Moussa Camara <msa.camara@gmail.com>
2023-10-26 14:33:46 +0200

    création variable et concatenation

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    // LES VARIABLES
+    $chaine = "COUCOU";
+
+    $nombre = 5;
+    $tab = array(5,"test");
+
+//Affichage des variables
+echo $chaine.'<br>';
+echo $nombre;
+
+$prenom = "Moussa";
+$nom = "CAMARA";
+echo $prenom;
+
+echo $nom;
+
+echo '<br> <br>';
+/*
+test 
+vernon
+DISSI
+*/
+
+
+
+// LES CONCATENATIONS
+$fullname = $prenom.' '.$nom;
+echo $fullname;
+
+echo $prenom.' '.$nom;
+?>
